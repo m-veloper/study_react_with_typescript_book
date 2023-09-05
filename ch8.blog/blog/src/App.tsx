edf70aa38e9f3768f27f4191979b0416a3242af4
m-veloper <m_veloper@naver.com>
2023-09-05 20:02:03 +0900

    ch9.storybook ing

--- ch8.blog/blog/src/App.tsx
+++ ch8.blog/blog/src/App.tsx
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { Header } from 'components/Header';
 import { BlogPost } from 'components/BlogPost';
+import { Button } from 'components/Button';
+import { Form } from 'components/Form';
 
 const Container = styled.div`
   height: 100vh;
@@ -13,11 +14,42 @@ const Container = styled.div`
   overflow: scroll;
 `;
 
+const ButtonContainer = styled.div`
+  position: absolute;
+  right: 40px;
+  bottom: 40px;
+`;
+
+interface Post {
+  readonly id: number;
+  readonly userId: number;
+  readonly title: string;
+  readonly body: string;
+}
+
 function App() {
+  const [posts, setPosts] = useState<ReadonlyArray<Post>>([]);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <Container>
-      <Header/>
-      <BlogPost title='asdasd' body='asdad'/>
+      <Header />
+      {posts.map((post) => (
+        <BlogPost key={post.id} title={post.title} body={post.body} />
+      ))}
+      <ButtonContainer>
+        <Button label="등록" onClick={() => setShowForm(true)} />
+      </ButtonContainer>
+      {showForm && <Form onClose={() => setShowForm(false)} />}
     </Container>
   );
 }
